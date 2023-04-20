a7687cb56577ba90d53fbbf2477e98c96f4e6d41
shintaro0930 <ozaki.shintaro@icloud.com>
2023-04-20 22:41:23 +0000

    today's first commit

--- morpheme.py
+++ morpheme.py
@@ -1,4 +1,4 @@
 import MeCab
-mecab = MeCab.Tagger ("-d /usr/lib/x86_64-linux-gnu/mecab/dic/mecab-ipadic-neologd/")
-testwords = "今日の天気は晴れです。"
+mecab = MeCab.Tagger ("-d /work/mecab-ipadic-neologd/build/mecab-ipadic-2.7.0-20070801-neologd-20200910/")
+testwords = "8月3日に放送された「中居正広の金曜日のスマイルたちへ」(TBS系)で、1日たった5分でぽっこりおなかを解消するというダイエット方法を紹介。キンタロー。のダイエットにも密着。"
 print(mecab.parse(testwords))
