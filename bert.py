d51abc0a2f5d0c59c3cf9571fcf91a18490a0425
shintaro0930 <ozaki.shintaro@icloud.com>
2023-04-29 13:51:30 +0000

    commit

--- bert.py
+++ bert.py
@@ -10,9 +10,12 @@ print(f"Using {device} device")
 model_name = 'cl-tohoku/bert-base-japanese'
 tokenizer = BertJapaneseTokenizer.from_pretrained(model_name)
 
-text ="私が目指すのは、新しい資本主義の実現です。成長を目指すことは極めて重要であり、その実現に向けて全力で取り組みます。"
+# text ="私が目指すのは、新しい資本主義の実現です。成長を目指すことは極めて重要であり、その実現に向けて全力で取り組みます。"
+text = "テレビでサッカーの試合をみる。"
 
 token = tokenizer.tokenize(text)
+masked_index = 2
+token[masked_index] = '[MASK]'
 # print(token)
 
 
@@ -25,19 +28,22 @@ tokens = tokenizer.convert_ids_to_tokens(input_ids)
 
 encoding = tokenizer(
 text, 
-max_length = 35, 
+max_length = 20, 
 padding ="max_length", 
 truncation=True,
 return_tensors="pt"
 )
 print("============")
 print(type(encoding))
-print(encoding['input_ids'])
 
 
-for encode in encoding:
-    print("===****===")
-    print(encoding[encode])
+tensor_encoding = encoding['input_ids']
+print(tensor_encoding)
+tensor_encoding = tensor_encoding.to(device)
+
+# for encode in encoding:
+#     print("===****===")
+#     print(encoding[encode])
 
 model = BertForSequenceClassification.from_pretrained(
     model_name,                     #日本語pre-trainedモデルの指定
@@ -49,3 +55,15 @@ model = BertForSequenceClassification.from_pretrained(
 #モデルをGPUへ
 model = model.to(device)
 
+
+# model.eval()      <--  これどういうこと?
+
+with torch.no_grad():
+    outputs = model(tensor_encoding)
+    print(outputs[0].shape)
+    predictions = outputs[0][0, masked_index].topk(5)
+
+for i, index_t in enumerate(predictions.indices):
+    index = index_t.item()
+    token = tokenizer.convert_ids_to_tokens([index])[0]
+    print(i, token)
