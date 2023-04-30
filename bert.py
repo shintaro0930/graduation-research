a04affb8d8070aa3772958ed4d28f040a2f37827
shintaro0930 <ozaki.shintaro@icloud.com>
2023-04-30 03:04:48 +0000

    made BERT model

--- bert.py
+++ bert.py
@@ -1,68 +1,55 @@
 from transformers import BertJapaneseTokenizer
-from transformers import BertModel
-from transformers import BertForSequenceClassification, AdamW, BertConfig
+from transformers import BertForMaskedLM
 import torch
+import logging
 
-print(torch.cuda.is_available())
+# warningの削除
+logging.getLogger("transformers.modeling_utils").setLevel(logging.ERROR)
+
 device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f"Using {device} device")
-
 model_name = 'cl-tohoku/bert-base-japanese'
 tokenizer = BertJapaneseTokenizer.from_pretrained(model_name)
 
-# text ="私が目指すのは、新しい資本主義の実現です。成長を目指すことは極めて重要であり、その実現に向けて全力で取り組みます。"
-text = "テレビでサッカーの試合をみる。"
+text ="私が目指すのは、新しい資本主義の実現です。成長を目指すことは極めて重要であり、その実現に向けて全力で取り組みます。"
+# text = "テレビでサッカーの試合をみる。"
 
 token = tokenizer.tokenize(text)
 masked_index = 2
+# Masked_tokenを2番目のトークンに置き換えるために先頭に[DUMMY]を挿入 / [CLS]トークンが消されないように
+token.insert(0, '[DUMMY]')
 token[masked_index] = '[MASK]'
-# print(token)
 
+print(f'masked_text: {token}')
 
-# BERTの入力データ
-input_ids = tokenizer.encode(text)
-print(input_ids)
-
-tokens = tokenizer.convert_ids_to_tokens(input_ids)
-# print(tokens)
-
+# Encoding
 encoding = tokenizer(
-text, 
-max_length = 20, 
-padding ="max_length", 
-truncation=True,
-return_tensors="pt"
+    text,                   # 変数textをエンコーディング
+    max_length = 20, 
+    padding ="max_length", 
+    truncation=True,
+    return_tensors="pt"
 )
-print("============")
-print(type(encoding))
 
-
+# Tensor型に変換して、GPUに送る
 tensor_encoding = encoding['input_ids']
-print(tensor_encoding)
 tensor_encoding = tensor_encoding.to(device)
 
-# for encode in encoding:
-#     print("===****===")
-#     print(encoding[encode])
-
-model = BertForSequenceClassification.from_pretrained(
-    model_name,                     #日本語pre-trainedモデルの指定
-    num_labels = 2,                 # ラベル数 (True/Falseの2択)
-    output_attentions = False,      # Attention Vectorを追加するかどうか
-    output_hidden_states = False,       # 隠れ層を出力するか
+# maskされたところの最大確率を求めるBERTモデル
+model = BertForMaskedLM.from_pretrained(
+    model_name,
+    output_attentions = False,
+    output_hidden_states = False,
 )
 
 #モデルをGPUへ
 model = model.to(device)
 
 
-# model.eval()      <--  これどういうこと?
-
 with torch.no_grad():
     outputs = model(tensor_encoding)
-    print(outputs[0].shape)
     predictions = outputs[0][0, masked_index].topk(5)
 
+print("=====\n[MASK]されたところに入る可能性の高い文字をtop5で出力する")
 for i, index_t in enumerate(predictions.indices):
     index = index_t.item()
     token = tokenizer.convert_ids_to_tokens([index])[0]
