3b64e2d5d72094238f8eee091b49eb10ba1b7439
shintaro0930 <ozaki.shintaro@icloud.com>
2023-05-11 04:16:32 +0000

    update files

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -5,13 +5,29 @@ import os
 import re
 
 """
-xml -> tsv
+xml -> csv
+
+参考リンク
+https://qiita.com/Robot-Inventor/items/2264590f392a1a9e8831
 """
+
+import re
 
 def convert_kanji_to_int(string):
     result = string.translate(str.maketrans("〇一二三四五六七八九", "0123456789", ""))
-    convert_table = {"十": "0", "百": "00", "千": "000", "万": "0000", "億": "00000000", "兆": "000000000000", "京": "0000000000000000"}
+    convert_table = {"十": "0", "百": "00", "千": "000", "万": "0000"}
     unit_list = "|".join(convert_table.keys())
+
+    # 年代
+    result = re.sub(r"年代(\D+)(\d+)", lambda x: f"年代{x.group(1)}{int(x.group(2))}", result)
+    # 年齢
+    result = re.sub(r"(\d+)歳", lambda x: str(int(x.group(1))), result)
+    # 金額
+    result = re.sub(r"(\d+)円", lambda x: str(int(x.group(1))), result)
+
+    # 年
+    result = re.sub(r"(\d+)年", lambda x: str(int(x.group(1))), result)
+
     while re.search(unit_list, result):
         for unit in convert_table.keys():
             zeros = convert_table[unit]
@@ -23,6 +39,7 @@ def convert_kanji_to_int(string):
                 result = result.replace(unit + number, "1" + zeros[len(number):len(zeros)] + number)
             result = result.replace(unit, "1" + zeros)
     return result
+
 
 def count_file(dir_path) -> int:
     count:int = 0
@@ -39,14 +56,16 @@ def main():
     # できれば上から順番になるようにしたい
 
     """rangeの日付は適宜変更"""
-    for i in range(1947, 2023):
+    for i in range(2022, 2023):
+#    for i in range(1947, 2023):
         file_paths:list = glob.glob('/work/data/' + str(i) + '_data/*.xml', recursive=True)
         for file in file_paths:
             tree = ET.parse(file)
             root = tree.getroot()
 
             try:
-                make_dir = '/work/csv_data/' + str(i) + '_data'
+                make_dir = '/work/tmp_data/' + str(i) + '_data'
+            #    make_dir = '/work/csv_data/' + str(i) + '_data'
                 if not os.path.exists(make_dir):
                     os.makedirs(make_dir)
             except Exception as e:
@@ -69,10 +88,10 @@ def main():
                     speaker, 
                     speaker_yomi, 
                     speaker_group, 
-                    speech
+                    convert_kanji_to_int(speech)
                 ])
-
-                with open('/work/csv_data/' + str(i) + '_data/' + str(date) + '.csv', mode='a') as f:
+                with open('/work/tmp_data/' + str(i) + '_data/' + str(date) + '.csv', mode='a') as f:
+#                with open('/work/csv_data/' + str(i) + '_data/' + str(date) + '.csv', mode='a') as f:
                     writer = csv.writer(f) 
                     writer.writerows(speech_list)
                 print(speech_list)
