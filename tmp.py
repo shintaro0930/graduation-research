4b92166b439002c1c7d93dc3e0c5770bb1b380f8
shintaro0930 <ozaki.shintaro@icloud.com>
2023-05-21 12:17:07 +0000

    update

--- tmp.py
+++
@@ -1,5 +0,0 @@
-import re
-
-with open("a.txt") as f:
-    result = re.sub(r"○(.*)　", "", f.readlines()[0])
-    print(result)
--- write_label.py
+++ write_label.py
@@ -7,32 +7,36 @@ def main():
     for i in range(2022, 2023):
         row = []
         # file_paths = glob.glob('/work/csv_data/' + str(i) + '_data/*.csv', recursive=True)
-        file_paths = glob.glob('/work/csv_data/' + str(i) + '_data/2022-01-07.csv', recursive=True)
-        for file in file_paths:
-            with open(file, 'r') as csv_read_file:
-                reader = csv.reader(csv_read_file)
-                rows = list(reader)
-            
-            try:
-                make_dir = '/work/label_data/' + str(i) + '_data'
-                if not os.path.exists(make_dir):
-                    os.makedirs(make_dir)
-            except Exception as e:
-                continue
+        file_paths = glob.glob('/work/csv_data/' + str(i) + '_data/2022-01-13.csv', recursive=True)
 
-            for row in rows:
-                print(row[8])
-                while True:
-                    user_input = input("1: 賛成, 0: 反対, 2: どちらでもない, 3: 議題を含むを入力してください: ")
-                    if user_input in ['0', '1', '2', '3']:
-                        print("入力された値:", user_input)
-                        row[7] = user_input 
-                        with open('/work/label_data/' + str(i) + '_data/2022-01-07.csv', 'a') as csv_write_file:
-                            writer = csv.writer(csv_write_file)
-                            writer.writerow(row)
-                        break
-                    else:
-                        print("無効な入力です")
+        for file in file_paths:
+            if file == '/work/csv_data/2022_data/2022-01-13.csv' or '/work/csv_data/2022_data/2022-01-07.csv':
+                continue
+            else:
+                with open(file, 'r') as csv_read_file:
+                    reader = csv.reader(csv_read_file)
+                    rows = list(reader)
+                
+                try:
+                    make_dir = '/work/label_data/' + str(i) + '_data'
+                    if not os.path.exists(make_dir):
+                        os.makedirs(make_dir)
+                except Exception as e:
+                    continue
+
+                for row in rows:
+                    print(row[8])
+                    while True:
+                        user_input = input("1: 賛成, 0: 反対, 2: どちらでもない, 3: 議題を含むを入力してください: ")
+                        if user_input in ['0', '1', '2', '3']:
+                            print("入力された値:", user_input)
+                            row[7] = user_input 
+                            with open('/work/label_data/' + str(i) + '_data/2022-01-13.csv', 'a') as csv_write_file:
+                                writer = csv.writer(csv_write_file)
+                                writer.writerow(row)
+                            break
+                        else:
+                            print("無効な入力です")
 
 if __name__ == "__main__":
     main()
