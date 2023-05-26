3b69e5229df1b5cf42a4f8fea041b7ee05ea762b
shintaro0930 <ozaki.shintaro@icloud.com>
2023-05-26 11:42:49 +0000

    update

--- write_label.py
+++ write_label.py
@@ -3,6 +3,24 @@ import csv
 import re
 import os
 
+
+def converter(string):
+    result = string.translate(str.maketrans("〇一二三四五六七八九", "0123456789", ""))
+    convert_table = {"十": "0", "百": "00", "千": "000", "万": "0000"}
+
+    for unit in convert_table.keys():
+        zeros = convert_table[unit]
+        for numbers in re.findall(f"(\d+){unit}(\d+)", result):
+            result = result.replace(
+                numbers[0] + unit + numbers[1], numbers[0] + zeros[len(numbers[1]):len(zeros)] + numbers[1])
+        for number in re.findall(f"(\d+){unit}", result):
+            result = result.replace(number + unit, number + zeros)
+        for number in re.findall(f"{unit}(\d+)", result):
+            result = result.replace(
+                unit + number, "1" + zeros[len(number):len(zeros)] + number)
+        result = result.replace(unit, "1" + zeros)
+    return result
+
 def main():
     csv_date = '2022-02-02.csv'
     for i in range(2022, 2023):
@@ -29,6 +47,21 @@ def main():
                     # 質問なのか、応答なのかの分類
                     # 正規表現で賛成なのか、反対なのか
                     # 議題の取得2
+
+                    # 漢数字 + 10分　が出てきたら適宜出力 
+                    # それをそのままにするのか or アラビア数字に直すか
+                    while True:
+                        if re.search('((〇|一|二|三|四|五|六|七|八|九|十|百|千|万)+)|(10分)', row[8]):
+                            user_input = input('1: アラビア数字に変更, 0: そのまま')
+                            if user_input == '1':
+                                converter(row[8])
+                                break
+                            elif user_input == '0':
+                                break
+                            else:  
+                                print('無効な入力です')
+                                
+
 
                     user_input = input("1: 賛成, 0: 反対, 2: どちらでもない, 3: 議題を含む: , 4:ゴミ: ")
                     if user_input in ['0', '1', '2', '3', '4']:
--- xml_to_csv.py
+++ xml_to_csv.py
@@ -36,49 +36,15 @@ def find_text(text, pattern):
     else:
         return None
 
-def labeling(text):
-    support_words = [
-        "賛成",
-        "賛同",
-        "賛決",
-        "同意",
-        "了解",
-        "可決",
-        "支持",
-        "肯定",
-        "好意的",
-        "賛唱",
-        "同感",
-        "理解",
-        "賛美",
-        "是認",
-        "賛成の立場から",
-        "支持する考え",
-        "前向きに受け止める",
-        "前向きな姿勢"
-    ]
+def get_label(text):
 
-    support_tokens = [nlp(w)[0] for w in support_words]
-
-    doc = nlp(text)
-
-    support_similarities = [cosine_similarity(t.vector.reshape(1, -1), support_token.vector.reshape(1, -1))[0][0] for t in doc for support_token in support_tokens]
-    average_cos_similarity = sum(support_similarities) / len(support_similarities)
-    print(f'{average_cos_similarity:.2f}')
-
-    plt.scatter(len(text), average_cos_similarity)
-    plt.xlim(0, 500)
-    plt.title(u'cos_sim between text and "pro"')
-    plt.xlabel(u'text size')
-    plt.ylabel(u'cos_sim')
-    plt.savefig('sample.png')
-    return average_cos_similarity
+    return 0
 
 
 def pattern_match(text):
     yen_regex = r"(〇|一|二|三|四|五|六|七|八|九|十|百|千|万)+(円|枚|人|名|億|兆|件|団体|代|店舗|週間|年間|日間|回|波|か月|年度)"
     per_regex = r"(〇|一|二|三|四|五|六|七|八|九)*(・)*(〇|一|二|三|四|五|六|七|八|九)(％|倍|億円|兆円)"
-    date_regex = r"(〇|一|二|三|四|五|六|七|八|九|十|千|百|万)+(月|日|歳|年|時|分|秒|つ)"
+    date_regex = r"(〇|一|二|三|四|五|六|七|八|九|十|千|百|万)+(月|日|歳|年|時|分|秒|つ|割|社|問)"
 
     yen_matches = re.finditer(yen_regex, text, re.MULTILINE)
     for match in yen_matches:
@@ -132,36 +98,73 @@ def main():
             except Exception as e:
                 continue
 
+            try:
+                make_dir = '/work/title_data/' + str(i) + '_data'
+                if not os.path.exists(make_dir):
+                    os.makedirs(make_dir)
+            except Exception as e:
+                continue            
+
             for record in root.iter(tag='speechRecord'):
                 speech_list = []
+                title_list = []
                 speaker = record.find('speaker').text
                 speaker_yomi = record.find('speakerYomi').text
                 speech = record.find('speech').text   
-                speech = re.sub(r"○(.*)　", "", speech)
-                speech = re.sub(r"(〔.*〕)|(（.*）)", "", speech)
+                speech = re.sub(r"○(.*)　|(〔.*〕)|（.*）|(─+)|(―+)|◇", "", speech).replace('　', '')
+                speech = pattern_match(speech)
                 date = record.find('date').text
                 speaker_group = record.find('speakerGroup').text
                 name_of_house = record.find('nameOfHouse').text
                 name_of_meeting = record.find('nameOfMeeting').text
-                if re.search(r'((―+)\◇(―+))|(―+)', speech):
-                    # マッチした場合はそのブロック全てを削除
+                
+                # kanji = re.findall(r"(((〇|一|二|三|四|五|六|七|八|九|十|千|百|万)+).?)", speech)
+                # if kanji:
+                #     print(kanji[0][0])
+
+                # マッチした場合はそのブロック全てを削除
+                #議題を取得できる                
+                if re.search(r'((―+)\◇(―+))|(―+)|((午前|午後)(\d*|零)(時)(\d*|零)(分)*(開会|休憩|閉会|散会|開議))', speech):
+                    pattern = r'本日の会議に付した案件\n([\s\S]*)'
+                    match = re.search(pattern, speech)
+    
+                    if match:
+                        result = match.group()
+                        result = re.sub('○|本日の会議に付した案件\n', '', result)
+
+                        title_list.append([
+                            date,
+                            name_of_house,
+                            name_of_meeting,
+                            speaker,
+                            speaker_yomi,
+                            speaker_group,
+                            '',
+                            '',
+                            result
+                        ])
+                        with open('/work/title_data/' + str(i) + '_data/' + str(date) + '.csv', mode='a') as f:
+                            writer = csv.writer(f)
+                            for title in title_list:
+                                writer.writerow(title)
+                            
                     continue
-                else:
-                    speech_list.append([
-                        date,
-                        name_of_house,
-                        name_of_meeting,
-                        speaker,
-                        speaker_yomi,
-                        speaker_group,
-                        '',     # 議題
-                        '',     # 賛成 or 反対 or それ以外
-                        pattern_match(speech)
-                    ])
-                    with open('/work/csv_data/' + str(i) + '_data/' + str(date) + '.csv', mode='a') as f:
-                        writer = csv.writer(f)
-                        for speech in speech_list:
-                            writer.writerow(speech)
+
+                speech_list.append([
+                    date,
+                    name_of_house,
+                    name_of_meeting,
+                    speaker,
+                    speaker_yomi,
+                    speaker_group,
+                    '',     # 議題
+                    '',     # 賛成 or 反対 or それ以外
+                    speech
+                ])
+                with open('/work/csv_data/' + str(i) + '_data/' + str(date) + '.csv', mode='a') as f:
+                    writer = csv.writer(f)
+                    for speech in speech_list:
+                        writer.writerow(speech)
 
 
 if __name__ == "__main__":
